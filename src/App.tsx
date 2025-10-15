bff404efcfee5be5cf91fd9ec9170e9d1dc67e1d
Pedro Pinheiro <59080094+PedroJiraya@users.noreply.github.com>
2025-10-15 14:43:41 +0000

    Feito o cabeçalho

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ListChecks } from "lucide-react";
+import * as C from "./App.styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <C.Container>
+    <C.Area>
+        <h1>Lista de Tarefas <ListChecks size={24} /></h1>
+    </C.Area>
+  </C.Container>;
+};
 
 export default App;
